f32937eba549a94ebf89c02fd165bff74ca6adf7
Harry Higgins <harrychiggins@gmail.com>
2025-11-03 23:31:19 +0000

    Initial Commit

---
+++ AudioTesting/AudioTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioTestingApp.swift
+//  AudioTesting
+//
+//  Created by Harry Higgins on 03/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct AudioTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
